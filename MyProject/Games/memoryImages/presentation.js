97b702592268eafca1e0765e02afacbef3c7b6f6
Dejan Zaprev <dzaprev@yahoo.com>
2019-09-16 02:35:30 +0200

    edited

--- MyProject/Games/memoryImages/presentation.js
+++ MyProject/Games/memoryImages/presentation.js
@@ -3,10 +3,9 @@ function Presentation() {
     this.businessObj = new BusinessLayer();
     this.imagesLev1 = [];
     this.imagesLev2 = [];
-    
+
     this.displayImagesLev1 = async () => {
         this.imagesLev1 = await this.businessObj.getImagesLevel1();
-        console.log(this.imagesLev1);
         return this.imagesLev1;
     }
 
